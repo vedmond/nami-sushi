1330ed25efa4103f126b0f8ee672ac1770af9deb
vedmond <vyedkh@gmail.com>
2024-03-06 08:37:44 +0300

    fix path link image

--- src/components/card-components/card-image-button.vue
+++ src/components/card-components/card-image-button.vue
@@ -1,12 +1,14 @@
 <script setup>
-import { inject } from 'vue'
+import { inject, ref } from 'vue'
 defineProps({
   style: String
 })
+const itemLink = ref('')
 const item = inject('item')
+itemLink.value = item.image_link
 </script>
 <template>
   <button :class="style">
-    <img :src="item.image_link" :alt="`${item.title}`" class="m-auto" />
+    <img :src="itemLink" :alt="`${item.title}`" class="m-auto" />
   </button>
 </template>
